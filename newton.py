c7db5a1711082978f8a61f005f1f6f886636c4be
Francisco Izurieta <franizus@live.com>
2018-02-15 17:28:55 -0500

    agregado funcion a string

--- newton.py
+++ newton.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def coef(x, y, n):
     fdd = np.zeros((n, n))
@@ -8,7 +8,6 @@ def coef(x, y, n):
     for j in range(1, n):
         for i in range(n - j):
             fdd[i][j] = (fdd[i+1][j-1] - fdd[i][j-1]) / (x[i+j] - x[i])
-    
     coefficients = np.zeros(n)
     for i in range(0, n):
         coefficients[i] = fdd[0][i]
@@ -42,33 +41,49 @@ def ff(xval, coeff, x):
             aux = aux + (coeff[j] * val)
         values.append(aux)
     return values
-        
 
-n = int(input('Ingrese el grado del polinomio: '))
-values = input('Ingrese los puntos x,y de la siguiente manera:\n\tEjemplo: x1:y1,x2:y2,...,xn:yn\n')
+def fs(coeff, x):
+    str_func = ''
+    for j in range(len(coeff)):
+        if j > 0:
+            if coeff[j] >= 0:
+                str_func += ' + ' + str(coeff[j])
+            else:
+                str_func += ' - ' + str(abs(coeff[j]))
+        else:
+            str_func += str(coeff[j])
+        for k in range(j):
+            if x[k] >= 0:
+                str_func += '(x' + ' - ' + str(x[k]) + ')'
+            else:
+                str_func += '(x' + ' + ' + str(abs(x[k])) + ')'
+    return str_func
+
+
+n = int(input('Ingrese el grado del polinomio: ')) + 1
+print('\nIngrese las coordenadas x,y de los puntos a analizar:\nEjemplo: x1:y1 > 3.5:2.6')
 x = []
 y = []
-auxstr = values.split(',')
-for val in auxstr:
-    aux = val.split(':')
+for i in range(n):
+    aux = input('x' + str(i) + ':y' + str(i) + ' > ').split(':')
     x.append(float(aux[0]))
     y.append(float(aux[1]))
 
 #x = [1, 4, 6, 5]
 #y = [0, 1.386294, 1.791759, 1.609438]
-print(x)
-print(y)
-xi = float(input('Ingrese el valor a evaluar: '))
+xi = float(input('\nIngrese el valor a evaluar: '))
 
 coefficients = coef(x, y, n)
 yval = eval(coefficients, x, xi, n)
 err = error(x, coefficients[n-1], xi)
-print(coefficients)
-print(yval)
-print(err)
-xt = np.arange(min(x), max(x), 0.1)
+
+print('\nf(x) = ' + fs(coefficients, x))
+print('\nf(' + str(xi) + ') = ' + str(yval))
+print('\nError = ' + str(err))
+
+'''xt = np.arange(min(x), max(x), 0.1)
 yt = ff(xt, coefficients, x)
 plt.plot(xt, yt)
 plt.plot(xi, yval, 'ro')
 plt.axis([min(x), max(x), min(y), max(y)])
-plt.show()
+plt.show()'''
